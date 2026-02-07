b49a653be3dc9fcd34dfcb2cca2921b761792059
farid <abbaslifarid2006@gmail.com>
2026-02-07 15:21:04 +0400

    Remove secrets from git

--- ASP_NET_03/Data/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ASP_NET_03._Dependency_Injection___services.Models;
-
-namespace ASP_NET_03._Dependency_Injection___services.Data;
-
-public interface IProductRepository
-{
-    public Product AddProduct(Product product);
-    public IEnumerable<Product> GetProducts();
-}
