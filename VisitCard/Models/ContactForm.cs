6105ef6fae0842fc50f0ebf151b4a6f157e1042f
Farid Abbasli <abbaslifarid2006@gmail.com>
2026-02-07 17:54:50 +0400

    Delete VisitCard directory

--- VisitCard/Models/ContactForm.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication2.Models;
-
-public class ContactForm
-{
-    public string Name { get; set; }=String.Empty;
-    public string Email { get; set; }=String.Empty;
-    public string Message { get; set; }=String.Empty;
-}
